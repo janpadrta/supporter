e1818a97e0cb2531823d3e25021b82ef11c67f9f
janpadrta <jan.padrta.82@gmail.com>
2024-05-24 17:18:03 +0200

    removing users from team

--- app/controllers/user_to_team_controller.rb
+++ app/controllers/user_to_team_controller.rb
@@ -1,29 +1,35 @@
 class UserToTeamController < ApplicationController
-  before_action :set_user, only: %i[ update destroy ]
   before_action :leader_required!
 
   # PATCH/PUT /users/1
   def update
-    p "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    @user = User.includes(:team).find(params[:user][:id])
+
     if @user.update(user_params)
-      redirect_to team_url(@user), notice: "Team was successfully updated."
+      redirect_to team_url(@user.team), notice: "User #{@user.name} has been added."
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
+    @user = User.includes(:team).find(params[:user_id])
+    team = @user.team
+
+    @user.update_attribute(:team_id, nil)
+    
+    redirect_to team_url(team), notice: "User #{@user.name} has been removed."
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_user
-    @user = User.includes(:team).find(params[:id])
+    @user = User.includes(:team).find(params[:user][:id])
   end
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:id, :team_id)
+    params.require(:user).permit(:team_id)
   end
 end
